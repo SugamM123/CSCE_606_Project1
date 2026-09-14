1b85df832b1124d3e64b3f7d5979cd81798902ac
Jason Atkins <jatkins11@tamu.edu>
2026-09-14 18:49:30 -0500

    Add checkout_book to Library and wire CLI action

--- lib/library.rb
+++ lib/library.rb
@@ -2,12 +2,16 @@
 
 require_relative 'book'
 require_relative 'member'
+require_relative 'loan'
 
 # manages books and members
 class Library
+  DEFAULT_LOAN_DAYS = 14
+
   def initialize
     @books = []
     @members = []
+    @loans = []
   end
 
   def add_book(title, author)
@@ -22,9 +26,7 @@ class Library
 
   def add_member(name, id)
     # check for duplicate member ID
-    if find_member(id)
-      raise ArgumentError, "Member with ID #{id} already exists"
-    end
+    raise ArgumentError, "Member with ID #{id} already exists" if find_member(id)
 
     member = Member.new(id: id, name: name)
     @members << member
@@ -40,6 +42,28 @@ class Library
     @members.find do |member|
       member.id == id || member.id.to_s == id.to_s
     end
+  end
+
+  def find_book(id)
+    @books.find { |book| book.id == id || book.id.to_s == id.to_s }
+  end
+
+  def checkout_book(book_id, member_id, due_date: Date.today + DEFAULT_LOAN_DAYS)
+    book = find_book(book_id)
+    member = find_member(member_id)
+
+    raise ArgumentError, "Book with ID #{book_id} not found" unless book
+    raise ArgumentError, "Member with ID #{member_id} not found" unless member
+    raise ArgumentError, "Book '#{book.title}' is already checked out" unless book.available?
+
+    book.status = Book::STATUS_CHECKED_OUT
+    loan = Loan.new(book_id: book.id, member_id: member.id, due_date: due_date)
+    @loans << loan
+    loan
+  end
+
+  def loans
+    @loans.dup
   end
 
   private
